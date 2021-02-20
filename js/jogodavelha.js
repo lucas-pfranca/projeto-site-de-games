fc403d90c84195167c62cc72c2b8cd42286799a0
Lucas Penalva <lucaspenalvacc123@gmail.com>
2021-02-19 21:23:41 -0300

    att jogo da velha

--- js/jogodavelha.js
+++ js/jogodavelha.js
@@ -1,10 +1,27 @@
 const tic_tac_toe = {
     board: ['','','','','','','','',''],
-    simbols:['X','O'],
+    simbols: {
+        options: ['X', 'O'],
+        turn_index: 0,
+        change: function() {
+            this.turn_index = (this.turn_index === 0 ? 1 : 0);
+        }
+    },
     container_element: null,
+    gameover: false,
 
     init: function(container){
         this.container_element = container;
+    },
+
+    make_play: function(position) {
+        if (this.gameover) {
+            return false
+        }
+        if (this.board[position] === '') {
+            this.board[position] = this.simbols.options [this.simbols.turn_index];
+            this.draw();
+        }
     },
 
 
@@ -12,7 +29,7 @@ const tic_tac_toe = {
         let content = '';
 
         for (i in this.board) {
-            content += '<div>' + i + '</div>';
+            content += '<div onclick = "tic_tac_toe.make_play(' + i + ')">' + this.board[i]
         }
 
         this.container_element.innerHTML =  content;
